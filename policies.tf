7ff9a5c322951536534c855a59dfba896d067bab
Gregory Vespasien <gregory.vespasien@curalie.com>
2022-08-03 19:10:52 +0200

    create drone s3 user, group and policy

--- policies.tf
+++ policies.tf
@@ -1,4 +1,4 @@
-# policies
+# iam full access policy
 resource "aws_iam_policy" "iam_full_access" {
   name        = var.groups[0].policy
   path        = "/"
@@ -41,11 +41,56 @@ resource "aws_iam_policy" "iam_full_access" {
   })
 }
 
+# s3 creation/deletion bucket policy
+resource "aws_iam_policy" "s3_creation_perm" {
+  name        = var.groups[1].policy
+  path        = "/"
+  description = "A policy allowing creating and deleting s3 buckets"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:CreateBucket",
+          "s3:DeleteBucket",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+        Condition = {
+          IpAddress = {
+            "aws:SourceIp" = [
+              "51.83.179.16/32",
+              "51.83.147.42/32"
+            ]
+          }
+        }
+      },
+      {
+        Action = [
+          "s3:*",
+        ]
+        Effect   = "Allow"
+        Resource = "arn:aws:s3:::gregentoo-terraform-backend/*"
+        Condition = {
+          IpAddress = {
+            "aws:SourceIp" = [
+              "51.83.179.16/32",
+              "51.83.147.42/32"
+            ]
+          }
+        }
+      },
+    ]
+  })
+}
+
 # this is needed to reference the policy in aws_iam_group_policy_attachment's arn
 data "aws_iam_policy" "policy" {
   for_each = { for group in var.groups: group.name => group }
   name = each.value.policy
   depends_on = [
-    aws_iam_policy.iam_full_access
+    aws_iam_policy.iam_full_access,
+    aws_iam_policy.s3_creation_perm
   ]
 }
