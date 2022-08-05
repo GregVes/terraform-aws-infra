22335416e47cc8d1c4d9f3ca8c9eeea4856baf1d
Gregory Vespasien <gregory.vespasien@curalie.com>
2022-08-05 10:11:43 +0200

    add bucket server side encryption

--- s3-buckets.tf
+++ s3-buckets.tf
@@ -16,3 +16,15 @@ resource "aws_s3_bucket_versioning" "bucket_versioning" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_encryption" {
+  for_each = toset(var.buckets)
+
+  bucket = each.key
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
