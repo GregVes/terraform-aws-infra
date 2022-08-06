e5576c93969a7bef528a49c669da9b9d6f08c054
Gregory Vespasien <gregory.vespasien@curalie.com>
2022-08-06 12:58:25 +0200

    add us-east-1 provider for ecr resource

--- ecr.tf
+++ ecr.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
 resource "aws_ecrpublic_repository" "repository" {
   provider = aws.us_east_1
   for_each = toset(var.ecr_repositories)
